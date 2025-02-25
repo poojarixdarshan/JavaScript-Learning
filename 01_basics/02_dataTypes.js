5d7ac3e920e38ec131013ed9a0306fbb7356002e
Darshan Poojari <92663196+poojarixdarshan@users.noreply.github.com>
2025-02-25 13:18:42 +0000

    commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3); // we are using nodejs not browser
+
+console.log("Darshan");
+
+let name = "Darshan";
+let age = 18;
+let isLoggedIn = true;
+
+//number => 2 to the power of 53
+//bigint
+//boolean true/false
+//string => ""
+//undefined 
+//null => standalone value
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
+
