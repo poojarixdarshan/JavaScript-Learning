09d310502ec349ac25aaf23357f73680ee769c05
Darshan Poojari <92663196+poojarixdarshan@users.noreply.github.com>
2025-02-25 07:28:52 +0000

    commit 2

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345;
+let accountEmail = "poojaridarshan12@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+//accountId = 2; //not allowed
+
+accountEmail = "hs@hs.com";
+accountPassword = "21212121";
+accountCity = "Bengaluru";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
