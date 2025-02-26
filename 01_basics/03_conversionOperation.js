a421df8f5cce79b46624991edb89a8c29c9c8cbe
Darshan Poojari <92663196+poojarixdarshan@users.noreply.github.com>
2025-02-26 08:31:17 +0000

    commit

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "Darshan";
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN //Not a Number
+// true => 1; false => 0
+
+let isLoggedIn = "Darshan";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => fasle
+// "Darshan" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+
+
+
