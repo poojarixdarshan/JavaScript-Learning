635fca4595230e43b15ff37040a489ec9ab1bd75
Darshan Poojari <92663196+poojarixdarshan@users.noreply.github.com>
2025-02-27 00:18:27 +0000

    commit

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,30 +1,57 @@
-let score = "Darshan";
+// let score = "Darshan";
 
-//console.log(typeof score);
-//console.log(typeof(score));
+// //console.log(typeof score);
+// //console.log(typeof(score));
 
-let valueInNumber = Number(score);
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// //console.log(typeof valueInNumber);
+// //console.log(valueInNumber);
 
-// "33" => 33
-// "33abc" => NaN //Not a Number
-// true => 1; false => 0
+// // "33" => 33
+// // "33abc" => NaN //Not a Number
+// // true => 1; false => 0
 
-let isLoggedIn = "Darshan";
+// let isLoggedIn = "Darshan";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-//console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// //console.log(booleanIsLoggedIn);
 
-// 1 => true; 0 => false
-// "" => fasle
-// "Darshan" => true
+// // 1 => true; 0 => false
+// // "" => fasle
+// // "Darshan" => true
 
-let someNumber = 33;
+// let someNumber = 33;
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+/********************** Operations *****************************/
+let value = 3;
+
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Darshan";
+
+let str3 = str1 + str2;
+//console.log(str3);
+
+let gameCounter = 100;
+++gameCounter
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
 
 
 
